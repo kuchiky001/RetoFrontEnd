58bfa781520cdc568ade5e8cad7e4ffd0b14dce8
kuchiky001 <josechuman37@gmail.com>
2019-04-24 10:25:29 -0500

    parte1

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true
+    firebase: {
+    apiKey: "AIzaSyA2p3ZGowiy6GGrRt4OwRqqFzi0wwPwHLE",
+    authDomain: "retofrontend.firebaseapp.com",
+    databaseURL: "https://retofrontend.firebaseio.com",
+    projectId: "retofrontend",
+    storageBucket: "retofrontend.appspot.com",
+    messagingSenderId: "319084194865"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: true
+    firebase: {
+    apiKey: "AIzaSyA2p3ZGowiy6GGrRt4OwRqqFzi0wwPwHLE",
+    authDomain: "retofrontend.firebaseapp.com",
+    databaseURL: "https://retofrontend.firebaseio.com",
+    projectId: "retofrontend",
+    storageBucket: "retofrontend.appspot.com",
+    messagingSenderId: "319084194865"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
